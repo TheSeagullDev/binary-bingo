8b0c1313f97c35b5f836e0f727a05fc9f21a1815
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2025-08-16 10:35:14 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    function getRandom(length) {
+        let random = "";
+        for(let i = 0; i < length; i++) {
+            if(Math.random() > 0.5) {
+                random += "0";
+            }
+            else {
+                random += "1";
+            }
+        }
+        return random;
+    }
+
+    let binary = $state("0000000");
+    let length = $state(8);
+</script>
+
+<h1>Binary Bingo!</h1>
+<p>{binary}</p>
+<input type="number" placeholder="length" bind:value={length}>
+<input type="button" value="Get New Binary" onclick={() => binary = getRandom(length)}>
