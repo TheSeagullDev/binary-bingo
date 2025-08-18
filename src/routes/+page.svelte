3b236fbc3dc5d1353155a86475ac8b5d317e583f
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2025-08-17 23:05:54 -0400

    add some basic functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,11 +12,40 @@
         return random;
     }
 
-    let binary = $state("0000000");
+    function binToDec(bin) {
+        let dec = 0;
+        for(let i = 0; i < bin.length; i++) {
+            let current = bin.charAt(bin.length - 1 - i);
+            if (current === "1") {
+                dec += Math.pow(2, i);
+            }
+        }
+        return dec;
+    }
+
+    function decToBin(dec, length) {
+        if(dec > Math.pow(2, length) - 1) {
+            throw new Error(`Decimal ${dec} cannot fit in length ${length}`);
+        }
+        else {
+            let bin = dec.toString(2);
+            return bin.padStart(length, "0");
+        }
+    }
+
     let length = $state(8);
+    let binary = $state("00000000");
+    let decimal = $derived(binToDec(binary));
+    
 </script>
 
-<h1>Binary Bingo!</h1>
-<p>{binary}</p>
-<input type="number" placeholder="length" bind:value={length}>
-<input type="button" value="Get New Binary" onclick={() => binary = getRandom(length)}>
+<div class="bg-sky-950 h-screen text-sky-50 flex justify-center items-center">
+    <div class="bg-indigo-700 p-8 rounded-xl shadow-2xl">
+        <h1>Binary Bingo!</h1>
+        <p>{binary}</p>
+        <p>{decimal}</p>
+        <label for="length">Length:</label>
+        <input id="length" class="bg-indigo-600 p-2 rounded-xl shadow-md" type="number" placeholder="length" bind:value={length}>
+        <button class="bg-indigo-600 p-2 rounded-xl shadow-md" onclick={() => binary = getRandom(length)}>Get New Binary</button>
+    </div>
+</div>
